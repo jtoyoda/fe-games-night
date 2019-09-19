10b83fd8d3728f5c4c06c4ed4518eab42e6851f2
Jamie Toyoda <jtoyoda@openhouse.ai>
2019-09-19 12:27:13 -0600

    Adding in hardcoded address for now

--- src/ui/components/events/EventGrid.tsx
+++ src/ui/components/events/EventGrid.tsx
@@ -66,12 +66,12 @@ export class EventGrid extends React.Component<IProps> {
       ) : (
         <GameDisplay event={event}/>
       );
-    const calendarDescription = this.props.game ? `We are playing ${this.props.game}` : 'The game has not been choosen yet';
+    const calendarDescription = this.props.game ? `We are playing ${this.props.game}` : 'The game has not been chosen yet';
     const dateMoment = moment(this.props.event.date);
     const calendarEvent = {
       title: this.props.event.name,
       description: calendarDescription,
-      location: '210 Bermuda Dr NW Calgary',
+      location: process.env.REACT_APP_ADDRESS,
       startTime: dateMoment.toISOString(true),
       endTime: dateMoment.add(4, 'hour').toISOString(true),
     };
