0c241c5a084ff26d72f76d5c4eda3eb524f5a8e1
Jamie Toyoda <jtoyoda@openhouse.ai>
2021-07-19 11:36:11 -0600

    Fixing message hover issue

--- src/ui/components/attendees/Attendee.tsx
+++ src/ui/components/attendees/Attendee.tsx
@@ -1,6 +1,5 @@
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, IconButton, Popover, Typography } from '@material-ui/core';
 import styles from 'ui/components/attendees/AttendeeCard.module.css';
-import Tooltip from '@material-ui/core/Tooltip';
 import MessageIcon from '@material-ui/icons/Message';
 import * as React from 'react';
 import { GamerAttending } from 'services/eventService';
@@ -12,6 +11,17 @@ interface IProps {
 
 export function Attendee(props: IProps) {
   const attendee = props.attendee
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+
   return (
     <div key={`${attendee.name}-${attendee.email}`}>
       <Grid container={true} direction={'row'} alignItems='center'>
@@ -25,10 +35,28 @@ export function Attendee(props: IProps) {
         </Grid>
         <Grid item={true}>
           {attendee.message &&
-          <Tooltip title={<Typography>{attendee.message}</Typography>} placement='right'>
-            <MessageIcon/>
-          </Tooltip>
+            <IconButton onClick={handlePopoverOpen}>
+              <MessageIcon/>
+            </IconButton>
+
           }
+          <Popover
+            id="mouse-over-popover"
+            open={open}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'center',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'center',
+              horizontal: 'left',
+            }}
+            onClose={handlePopoverClose}
+            disableRestoreFocus
+          >
+            <Typography>{attendee.message}</Typography>
+          </Popover>
         </Grid>
       </Grid>
     </div>
